379c1875ac618e0cf4ec6371fd6b16b84ac93269
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-03-09 17:59:53 +0200

    update profile page

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -3,10 +3,32 @@ import Link from 'next/link';
 import css from './ProfilePage.module.css';
 import Image from 'next/image';
 import { useAuthStore } from '@/lib/store/authStore';
+import { Metadata } from 'next';
 
 //Для коректної роботи з віддаленими зображеннями у Next.js (аватар профілю)
 //  потрібно в next.config.ts додати розділ images з масивом remotePatterns,
 // який обов’язково містить hostname: 'ac.goit.global'.
+
+//: Metatags
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: 'Profile',
+    description: 'Page of the user profile',
+    openGraph: {
+      title: 'Profile',
+      description: 'Page of the user profile',
+      url: 'https://08-zustand-eight-beta.vercel.app',
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 640,
+          height: 640,
+          alt: 'NoteHub Logo image',
+        },
+      ],
+    },
+  };
+}
 
 const PrivatProfile = () => {
   const { user } = useAuthStore();
