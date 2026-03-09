4f31417f23e52c88ba6da2cea5294c5418689d5f
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-03-09 17:35:52 +0200

    test

--- app/(private routes)/notes/[id]/loading.tsx
+++ app/(private routes)/notes/[id]/loading.tsx
@@ -1,4 +1,4 @@
-import css from "@/app/notes/[id]/Loading.module.css";
+// import css from './Loading.module.css';
 const Loading = () => {
   return <h2 className={css.loading}>Завантаження деталей...</h2>;
 };
